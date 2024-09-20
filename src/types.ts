8f63292a5bfbf891061db1516e32d97b5e8f0b7d
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2024-09-20 15:09:16 +0200

    chore: format using biome

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,18 @@
-export type KeypairType = 'ed25519' | 'sr25519' | 'ecdsa' | 'ethereum'
+export type KeypairType = "ed25519" | "sr25519" | "ecdsa" | "ethereum";
 
 export interface UNIQUE_SDK_UnsignedTxPayloadBody {
-  signerPayloadJSON?: any
-  signerPayloadRaw?: any
-  signerPayloadHex: string
+	signerPayloadJSON?: any;
+	signerPayloadRaw?: any;
+	signerPayloadHex: string;
 }
 
 export interface UNIQUE_SDK_SignTxResultResponse {
-  signature: string
-  signatureType: KeypairType
+	signature: string;
+	signatureType: KeypairType;
 }
 
 export interface IUniqueSdkSigner {
-  sign: (unsignedTxPayload: UNIQUE_SDK_UnsignedTxPayloadBody) => Promise<UNIQUE_SDK_SignTxResultResponse>
+	sign: (
+		unsignedTxPayload: UNIQUE_SDK_UnsignedTxPayloadBody,
+	) => Promise<UNIQUE_SDK_SignTxResultResponse>;
 }
